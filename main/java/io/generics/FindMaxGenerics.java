3eb73cf225340f70313c6d7dbafe49b628997aea
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-10-11 19:49:08 +0530

    Added : Functionality to Find Max of Integers and TestCases

---
+++ main/java/io/generics/FindMaxGenerics.java
@@ -0,0 +1,23 @@
+package io.generics;
+
+public class FindMaxGenerics {
+	
+		/**
+		 * UC 1 Given 3 Integers find the maximum
+		 * @param first
+		 * @param second
+		 * @param third
+		 * @return
+		 */
+		public Integer maxNumInt(Integer first, Integer second, Integer third) {
+			if(first.compareTo(second) > 0 && first.compareTo(third) > 0)
+				return first;
+			else if(second.compareTo(first) > 0 && second.compareTo(third) > 0)
+				return second;
+			else return third;
+		}		
+}
+	
+
+
+
---
+++ test/java/io/generics/FindMaxGenericsTest.java
@@ -0,0 +1,43 @@
+package io.generics;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FindMaxGenericsTest {
+	
+	public static FindMaxGenerics findMaxGenerics;
+	
+	@BeforeEach
+	public void initiate() {
+		findMaxGenerics = new FindMaxGenerics();
+	}
+
+	@Test
+	public void givenFirstMax_shouldReturn_first() {
+		Integer first = 9;
+		Integer second = 5;
+		Integer third = 7;
+		assertEquals(first, findMaxGenerics.maxNumInt(first, second, third));
+	}
+	
+	@Test
+	public void second() {
+		Integer first = 9;
+		Integer second = 10;
+		Integer third = 7;
+		assertEquals(second, findMaxGenerics.maxNumInt(first, second, third));
+	}
+	
+	@Test
+	public void givenThirdMax_shouldReturn_third() {
+		Integer first = 9;
+		Integer second = 5;
+		Integer third = 10;
+		assertEquals(third, findMaxGenerics.maxNumInt(first, second, third));
+	}
+}	
+
+	
+
+
